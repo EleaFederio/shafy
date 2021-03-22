768a36333755ffe11023c779754441d5ca9dcc6e
EleazarFederio <eleazarfederio@rocketmail.com>
2021-03-22 23:54:23 +0800

    Add simple Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shopy/config/myColor.dart';
+import 'package:shopy/providers/Products.dart';
 import 'package:shopy/screens/product_detail_screen.dart';
 import 'package:shopy/screens/product_overview_screen.dart';
 
@@ -12,18 +14,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: MaterialColor(0xFF3d3e3c, color),
-        accentColor: Color(0xFFcde6fd),
-        fontFamily: 'Lato',
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: MaterialColor(0xFF3d3e3c, color),
+          accentColor: Color(0xFFcde6fd),
+          fontFamily: 'Lato',
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (context) => ProductDetailScreen()
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (context) => ProductDetailScreen()
-      },
     );
   }
 }
