4ffbc2f96564eb7c6e2d9eadaabc88182315cdee
Eleazar Federio <eleazarfederio@rocketmail.com>
2021-04-27 23:04:19 +0800

    add order product list animation

--- lib/screens/order_item.dart
+++ lib/screens/order_item.dart
@@ -20,63 +20,75 @@ class _OrderItemScreenState extends State<OrderItemScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10.0),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text('₱ ${widget.order.amount}'),
-            subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
-            trailing: IconButton(
-              icon: Icon(_expander ? Icons.expand_less : Icons.expand_more),
-              onPressed: (){
-                setState(() {
-                  _expander = !_expander;
-                });
-              },
-            ),
-          ),
-          if(_expander) Container(
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4.0),
-            height: min(
-              widget.order.products.length * 20/0 + 10.0,
-              100
-            ),
-            child: ListView(
-              children: widget.order.products.map((prod) =>
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      prod.title,
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    Text(
-                        '${prod.quantity} X ₱${prod.price}',
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        color: Theme.of(context).primaryColor
-                      ),
-                    ),
-                  ],
-                )
-              ).toList(),
-            ),
-          ),
-          FlatButton(
-            color: Theme.of(context).errorColor,
-            onPressed: (){},
-            child: Text(
-              'Cancel Order',
-              style: TextStyle(
-                color: Colors.white
+    return AnimatedContainer(
+      duration: Duration(
+        milliseconds: 300
+      ),
+      height: _expander ?  min(
+          widget.order.products.length * 20/0 + 110.0,
+          200
+      ): 95,
+      child: Card(
+        margin: EdgeInsets.all(10.0),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text('₱ ${widget.order.amount}'),
+              subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
+              trailing: IconButton(
+                icon: Icon(_expander ? Icons.expand_less : Icons.expand_more),
+                onPressed: (){
+                  setState(() {
+                    _expander = !_expander;
+                  });
+                },
               ),
             ),
-          )
-        ],
+            AnimatedContainer(
+              duration: Duration(
+                milliseconds: 300
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4.0),
+              height: _expander ?  min(
+                  widget.order.products.length * 20/0 + 10.0,
+                  100
+              ): 0,
+              child: ListView(
+                children: widget.order.products.map((prod) =>
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        prod.title,
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Text(
+                          '${prod.quantity} X ₱${prod.price}',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Theme.of(context).primaryColor
+                        ),
+                      ),
+                    ],
+                  )
+                ).toList(),
+              ),
+            ),
+            // FlatButton(
+            //   color: Theme.of(context).errorColor,
+            //   onPressed: (){},
+            //   child: Text(
+            //     'Cancel Order',
+            //     style: TextStyle(
+            //       color: Colors.white
+            //     ),
+            //   ),
+            // )
+          ],
+        ),
       ),
     );
   }
